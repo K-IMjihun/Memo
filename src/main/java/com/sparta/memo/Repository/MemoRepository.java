625bdb6e3010e6b07d277d1a8b96f3579006b61f
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-27 11:48:08 +0900

    Query Methods

--- src/main/java/com/sparta/memo/Repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/Repository/MemoRepository.java
@@ -3,9 +3,12 @@ package com.sparta.memo.Repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MemoRepository extends JpaRepository<Memo, Long>{
 
-
+    // SQL을 사용하지 않아도 메서드 이름을 읽고 자동으로 생성해줌
+    List<Memo> findAllByOrderByModifiedAtDesc();
     }
 
